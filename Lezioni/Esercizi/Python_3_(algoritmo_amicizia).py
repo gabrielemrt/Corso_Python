e290d632a8a0f6d92f8f8c9e781e44e49c85e7fd
gabrielemrt <gabriele.martini.03@gmail.com>
2022-11-30 21:25:13 +0100

    update 1.1
    update Python_3_(algoritmo_amicizia)

--- Lezioni/Esercizi/Python_3_(algoritmo_amicizia).py
+++ Lezioni/Esercizi/Python_3_(algoritmo_amicizia).py
@@ -1,28 +1,60 @@
 risposta = ""
 
+def verifica(a):
+    if (a=="Y") or (a=="y") or (a=="S") or (a=="s"):
+        return "Y"
+    elif (a=="N") or (a=="n"):
+        return "N"
+    else:
+        return "error"
+    
+
 print("\n\n")
-input("COMPONI IL NUMERO DI TELEFONO DELLA PERSONA: ")
-print("E' IN CASA?")
+input(">> COMPONI IL NUMERO DI TELEFONO DELLA PERSONA: ")
+print(">> E' IN CASA?")
 risposta = input("Rispondi con Y se sì o N se no: ")
-if risposta=="N":
-    print("LASCIA UN MESSAGGIO")
-    print("ASPETTA DI ESSERE RICHAMATO")
-risposta = input("TI VA DI MANGIARE QUALCOSA INSIEME? ")
-if risposta=="Y":
-    print("MANGIATE QUALCOSA INSIEME")
-elif risposta=="N":
-    risposta = input("E DI BERE QUALCOSA DI CALDO? ")
-    if risposta=="Y":
-        print("SCEGLI:")
-        print("TE', CAFFE' o CIOCCOLATA")
+conferma = verifica(risposta)
+if conferma=="N":
+    print(">> LASCIA UN MESSAGGIO")
+    print(">> ASPETTA DI ESSERE RICHAMATO")
+risposta = input(">> TI VA DI MANGIARE QUALCOSA INSIEME? ")
+conferma = verifica(risposta)
+if conferma=="Y":
+    print(">> MANGIATE QUALCOSA INSIEME")
+elif conferma=="N":
+    risposta = input(">> E DI BERE QUALCOSA DI CALDO? ")
+    conferma = verifica(risposta)
+    if conferma=="Y":
+        print(">> SCEGLI:")
+        print(">> TE', CAFFE' o CIOCCOLATA")
         risposta=input()
-        if risposta=="TE'": 
-            print("FATEVI STO ", risposta)
+        conferma = verifica(risposta)
+        if conferma=="TE'": 
+            print(">> FATEVI STO ", risposta)
         else:
-            print("FATEVI STA ", risposta)
-    elif risposta=="N":
-        print("")        
+            print(">> FATEVI STA ", risposta)
+    elif conferma=="N":
+        n=0
+        print(">> ALLORA GIOCHIAMO UN PO'...")
+        
+        while conferma!="Y":
+            print(">> ... COS'ALTRO TI VA DI FARE?")
+            risposta=input()
+            risposta=input(">> E' UNA COSA CHE VA DI FARE ANCHE A TE? ")
+            conferma = verifica(risposta)
+            if conferma=="Y":
+                print(">> E FACCIAMOLO INSIEME DAI...")
+            elif conferma=="N":
+                n=n+1
+                if n>6:
+                    print(">> SCEGLI FRA TUTTE LE OPZIONI QUELLA CHE TI SEMBRA ESSERE LA MENO DISUMANA")
+                    print(">> FATTELA PIACEGE")
+                    break
+        
+        print(">> SVAGATEVI UN PO' INSIEME")
+                
 
 
-print("SIETE DIVENTATI AMICI!")
-print("ORA HAI UNA PERSONA IN PIU' A CUI POTER ROMPERE LE PALLE IN CASO DI BISOGNO. E VICEVERSA")
+print("\n>> SIETE DIVENTATI AMICI!")
+print(">> ORA HAI UNA PERSONA IN PIU' A CUI POTER ROMPERE LE PALLE IN CASO DI BISOGNO. E VICEVERSA")
+print("\n\n")
